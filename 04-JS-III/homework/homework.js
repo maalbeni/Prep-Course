a088abe5f7e8159010966cf1abc61918615319b3
maalbeni <manuelbellonino@gmail.com>
2022-12-29 20:40:21 -0300

    completed test

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -2,19 +2,22 @@
 
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
-  // Tu código:
+  // Tu código:;
+  return array[0] ;
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+return array[array.length -1] ;
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length ;
 }
 
 
@@ -23,6 +26,8 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+ const a = array.map (x => x + 1);
+ return a ;
 }
 
 
@@ -30,6 +35,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push (elemento) ;
+  return array ;
 }
 
 
@@ -38,6 +45,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift (elemento) ;
+  return array ;
 }
 
 
@@ -47,6 +56,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return (palabras.join(' '));
 }
 
 
@@ -54,6 +64,7 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  return (array.includes(elemento));
 }
 
 
@@ -61,6 +72,9 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+const sum = numeros.reduce((a,v) => {return a + v;
+}, 0 );
+return sum
 }
 
 
@@ -68,6 +82,10 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  const sum = resultadosTest.reduce((a,v) => {return a + v;
+  }, 0 );
+  var tsum = sum / (resultadosTest.length) ;
+  return tsum;
 }
 
 
@@ -75,6 +93,8 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  const g = Math.max(...numeros);
+  return g ;
 }
 
 
@@ -82,13 +102,25 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+if (arguments.length < 1) return 0;
+var t = 1
+for (var i = 0 ; i< arguments.length ; i ++){
+  t = t * arguments[i];
+  }
+  return t;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+let c = 0
+for  (let i = 0 ; i < arreglo.length ; i ++){
+  if (arreglo[i] > 19){
+    c ++
+  }
+}
+return c
 }
 
 
@@ -97,7 +129,10 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if(numeroDeDia === 1 || numeroDeDia === 7 ){
+    return "Es fin de semana"
+  }
+  return "Es dia Laboral"
 } 
 
 
@@ -105,7 +140,11 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  let f = n.toString() 
+  if (f.charAt(0) === "9" ){
+    return true 
+  }
+  return false 
 }
 
 
@@ -113,7 +152,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for (let v = 0 ; v < arreglo.length ; v ++){
+    if (arreglo[v] === arreglo[v+1]){
+      return true
+    }
+  }
+return false
 } 
 
 
@@ -122,13 +166,30 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
-}
+  var na =[]
+  for (var i =0 ; i < array.length ; i ++){
+    if (array[i] === "Enero" || array[i] === "Marzo" || array[i] === "Noviembre"){
+      na.push(array[i])      
+     }
+   }
+   if (na.length < 3) {
+    return "No se encontraron los meses pedidos"
+   }
+   else return na
+  }
 
 
 function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var na = []
+  for (let i = 0 ; i < array.length ; i ++){
+    if (array[i] > 100 ){
+      na.push(array[i])
+    }
+  }
+  return na
 }
 
 
@@ -140,6 +201,21 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var na = [] ;
+  var count = numero ;
+  for (var i = 0 ; i < 10 ; i++){
+    count = count + 2;  
+    if (count === i) break ;
+    else {
+      na.push(count) ;
+    }
+  }
+  if (i < 10) {
+    return "Se interrumpió la ejecución" ;
+  }
+  else {
+     return na ;
+  }
 }
 
 
@@ -150,6 +226,16 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+  var na = [] ;
+  var count = numero ;
+  for (var i = 0 ; i < 10 ; i++){
+    if (i ===5) continue;
+    else {
+      count = count + 2
+      na.push (count)
+    }
+  }
+  return na ;
 }
 
 
